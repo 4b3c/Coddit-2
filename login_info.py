39a0cf518e5fadb04d77f70487b7151d3ff5acb2
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2021-09-14 18:19:50 -0700

    re added json file

--- login_info.py
+++ login_info.py
@@ -2,8 +2,8 @@ import praw
 
 #create the reddit instance
 reddit = praw.Reddit(
-    client_id = "client_id",
-    client_secret = "client_secret",
-    password = "password",
-    user_agent = "user_agent",
-    username = "username")
+    client_id = "",
+    client_secret = "",
+    password = "",
+    user_agent = "",
+    username = "")
